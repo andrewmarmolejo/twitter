0a4530a77dd8d80d9f82769db20d90c0e61b34d1
andrewmarmolejo <35593189+andrewmarmolejo@users.noreply.github.com>
2020-02-23 13:30:42 -0800

    Completed Part 2 - 02/23/2020

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -19,6 +19,12 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
+    }
+    
+    
     func loadTweets(){
         
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
@@ -64,9 +70,9 @@ class HomeTableViewController: UITableViewController {
         }
         
         
-        
-        
-        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         
         return cell
